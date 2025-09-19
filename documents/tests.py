5f6b5cbd47c9ee77520cc39fc8248efc9406be00
Olga <nefertituolga@gmail.com>
2025-09-19 21:21:49 +0300

    add fixes documents/tests.py

--- documents/tests.py
+++ documents/tests.py
@@ -131,15 +131,8 @@ class DocumentTestCase(APITestCase):
         response = self.client.get(url)
         print(f"👻 Аноним - Status: {response.status_code}")
 
-        if response.status_code == 401:
-            # Для 401 проверяем сообщение об аутентификации
-            self.assertIn("учетные данные", response.json().get("detail", "").lower())
-            self.assertIn("не были предоставлены", response.json().get("detail", "").lower())
-        elif response.status_code == 403:
-            # Для 403 проверяем сообщение о правах
-            self.assertIn("permission", response.json().get("detail", "").lower())
-            # или для русского сообщения:
-            self.assertIn("прав", response.json().get("detail", "").lower())
+        self.assertIn(response.status_code, [status.HTTP_401_UNAUTHORIZED, status.HTTP_403_FORBIDDEN])
+        self.assertTrue(response.status_code in [401, 403])
 
     def test_documents_list(self) -> None:
         """Тест списка документов"""
